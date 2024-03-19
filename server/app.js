50d7775437a5142838dd42016ce9247dc11a7a9b
Shrey Kumar <shreykumar191@gmail.com>
2024-03-19 20:10:33 +0530

    database connection setup

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 // import express from 'express'
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./database/db');
 const app = express();
+dotenv.config();
+const mongodb_url = process.env.MONGO_URI
 
 
 app.get('/',(req,res)=>{
@@ -8,6 +12,15 @@ app.get('/',(req,res)=>{
 })
 
 const PORT = 3000;
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+
+const start = async () => {
+    try {
+        await connectDB(mongodb_url);
+        app.listen(PORT,()=>{
+            console.log(`Server is running on port ${PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
