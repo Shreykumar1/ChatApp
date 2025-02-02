bdb9768ede223cf100b7cb2f1a3370ff17a67897
Shrey Kumar <shreykumar854@gmail.com>
2025-02-02 22:49:46 +0530

    Production Changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,5 +13,9 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    }
   },
 })
